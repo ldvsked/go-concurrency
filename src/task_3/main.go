dcd0afe442d2c9a81340e6af6a993d600b0e508e
authentk <authentk@msk-school.21>
2026-06-27 18:35:54 +0300

    feat: all 3

--- src/task_3/main.go
+++ src/task_3/main.go
@@ -4,7 +4,11 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"os"
+	"os/signal"
 	"strconv"
+	"sync"
+	"syscall"
 	"time"
 )
 
@@ -22,11 +26,28 @@ func readArgumentK() (uint, error) {
 	return uint(k), nil
 }
 
-func ticker(k int) {
-	for i := 0; i < k; i++ {
-		// сколько наносекунд в секунде(число) * количество секунд(но в нано, поэтому надо умножить)
-		time.Sleep(time.Second * time.Duration(k))
-		
+func ticker(k int, stop chan os.Signal, wg *sync.WaitGroup) {
+	for i := 0; ; i++ {
+		// до сна
+		select { // не стопается если канал пуст
+		case <-stop:
+			fmt.Println("Termination")
+			wg.Done()
+			return
+		default:
+			// сколько наносекунд в секунде(число) * количество секунд(но в нано, поэтому надо умножить)
+			time.Sleep(time.Second * time.Duration(k))
+		}
+
+		// после сна
+		select {
+		case <-stop:
+			fmt.Println("Termination")
+			wg.Done()
+			return
+		default:
+			fmt.Printf("Tick %d since %d\n", i+1, (i+1)*k)
+		}
 	}
 }
 
@@ -38,6 +59,19 @@ func main() {
 		return
 	}
 
-	
+	var stop chan os.Signal = make(chan os.Signal, 1) // cap
+	// SIGINT(Ctrl+C), SIGTERM - kill
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
 
+	var stopTicker chan os.Signal = make(chan os.Signal, 1)
+	var val os.Signal
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go ticker(int(k), stopTicker, &wg) // сказано что тиккер не блокирует мейн поэтому горутина
+
+	val = <-stop // ждет пока что-то придет
+	stopTicker <- val
+	// нужно подождать пока тиккер завершится
+	wg.Wait()
 }
